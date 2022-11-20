134f3bbbb192035cd25ebafe5def67cb4e39bbb8
Evgeny Shestak <107277013+Karavang@users.noreply.github.com>
2022-11-20 12:12:52 +0100

    early pull,which add 3*3 structure

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery')
 
-console.log(galleryItems);
+galleryItems.forEach(({preview,description})=>{
+const img = document.createElement('img')
+img.src = preview;
+img.alt= description;
+img.style.cssText='width:360px;height:240px;'
+gallery.append(img);
+})
+import * as basicLightbox from 'basiclightbox'
+
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`)
+
+instance.show()
